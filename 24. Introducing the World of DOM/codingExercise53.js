c5cd5b91117a935321ba01766025aa328179bbfc
Batista <jorge.lsbatista@gmail.com>
2022-03-06 22:56:45 -0300

    section 24 exercises resolution (until 54th exercise)

---
+++ 24. Introducing the World of DOM/codingExercise53.js
@@ -0,0 +1,3 @@
+// YOUR CODE GOES IN HERE:
+const pickles = document.querySelector('span');
+pickles.innerText = 'Disgusting';
---
+++ 24. Introducing the World of DOM/codingExercise54.js
@@ -0,0 +1,4 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+const chickenEgg = document.querySelector('img');
+chickenEgg.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg');
+chickenEgg.setAttribute('alt', 'chicken');
