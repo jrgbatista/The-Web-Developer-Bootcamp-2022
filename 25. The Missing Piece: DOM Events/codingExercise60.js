8bf8cca025a1a2d2d0fd0952fd128f9f44836180
Batista <jorge.lsbatista@gmail.com>
2022-03-07 15:14:23 -0300

    finishing exercises and starting the 25th module

---
+++ 25. The Missing Piece: DOM Events/codingExercise60.js
@@ -0,0 +1,9 @@
+const hello = document.querySelector('#hello');
+hello.addEventListener('click', function () {
+  console.log('hello');
+});
+
+const goodbye = document.querySelector('#goodbye');
+goodbye.addEventListener('click', function () {
+  console.log('goodbye');
+});
